7a8a72e850a2a508fa9fe16c768fca170c7ff9df
Maciel <macil_rj@hotmail.com>
2014-10-22 18:53:20 -0200

    restauração de arquivos corrompidos

--- layout/js/medidas.js
+++ layout/js/medidas.js
@@ -1,18 +1,17 @@
-/*
- * 
+/* 
  * medidas.js
  */
 
-
 var spanLargura = document.getElementById('largura');
 var spanAltura = document.getElementById('altura');
-//pega a largura da janela
-spanLargura.innerHTML = window.innerWidth + 'px';
-//pega a altura da janela
-spanAltura.innerHTML = window.innerHeight + 'px';
 
+//Pega a largura da janela
+spanLargura.innerHTML = window.innerWidth + ' px';
+//Pega a altura da janela
+spanAltura.innerHTML = window.innerHeight + ' px';
 
+//Ao redimensionar a janela
 window.onresize = function() {
-    spanLargura.innerHTML = window.innerWidth + 'px';
-    spanAltura.innerHTML = window.innerHeight + 'px';
+    spanLargura.innerHTML = window.innerWidth + ' px';
+    spanAltura.innerHTML = window.innerHeight + ' px';
 };
