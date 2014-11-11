057bfe7ca0a889015532c0db8c9659e282a734d8
Maciel <macil_rj@hotmail.com>
2014-11-11 21:49:18 -0200

    aula42 - Crud de usuários completo e integrado ao Banco de dados #5

--- application/views/usuarios_view.php
+++ application/views/usuarios_view.php
@@ -12,9 +12,50 @@
         <label for="nome">Nome:</label><br/>
         <input type="text" name="nome" value="<?php echo set_value('nome'); ?>"/>
         <div class="error"><?php echo form_error('nome'); ?></div>
-        <label for="email">Email:</label><br/>
+        
+        <label for="email">E-mail:</label><br/>
         <input type="text" name="email" value="<?php echo set_value('email'); ?>"/>
         <div class="error"><?php echo form_error('email'); ?></div>
+        
+        <label for="senha">Senha:</label><br/>
+        <input type="password" name="senha" value="<?php echo set_value('senha'); ?>"/>
+        <div class="error"><?php echo form_error('senha'); ?></div>
+        
+        <label for="dtnascimento">Data de Nascimento:</label><br/>
+        <input type="text" name="dtnascimento" value="<?php echo set_value('dtnascimento'); ?>"/>
+        <div class="error"><?php echo form_error('dtnascimento'); ?></div>
+        
+        <label for="foto">Foto:</label><br/>
+        <input type="text" name="foto" value="<?php echo set_value('foto'); ?>"/>
+        <div class="error"><?php echo form_error('foto'); ?></div>
+        
+        <label for="cidade">Cidade:</label><br/>
+        <input type="text" name="cidade" value="<?php echo set_value('cidade'); ?>"/>
+        <div class="error"><?php echo form_error('cidade'); ?></div>
+        
+        <label for="estado">Estado:</label><br/>
+        <input type="text" name="estado" value="<?php echo set_value('estado'); ?>"/>
+        <div class="error"><?php echo form_error('estado'); ?></div>
+        
+        <label for="bairro">Bairro:</label><br/>
+        <input type="text" name="bairro" value="<?php echo set_value('bairro'); ?>"/>
+        <div class="error"><?php echo form_error('bairro'); ?></div>
+        
+        <label for="endereco">Endereço:</label><br/>
+        <input type="text" name="endereco" value="<?php echo set_value('endereco'); ?>"/>
+        <div class="error"><?php echo form_error('endereco'); ?></div>
+        
+        <label for="cep">CEP:</label><br/>
+        <input type="text" name="cep" value="<?php echo set_value('cep'); ?>"/>
+        <div class="error"><?php echo form_error('cep'); ?></div>
+        
+        <label for="telefone">Telefone:</label><br/>
+        <input type="text" name="telefone" value="<?php echo set_value('telefone'); ?>"/>
+        <div class="error"><?php echo form_error('telefone'); ?></div>
+        
+        <label for="celular">Celular:</label><br/>
+        <input type="text" name="celular" value="<?php echo set_value('celular'); ?>"/>
+        <div class="error"><?php echo form_error('celular'); ?></div>
         <input type="submit" name="cadastrar" value="Cadastrar" />
 
         <?php echo form_close(); ?>
